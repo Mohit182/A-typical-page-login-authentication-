3008d2e424ea25616a44a2429b69731ed4f6345c
Mohit Gupta <mohitguptatech18@gmail.com>
2021-11-19 07:58:55 +0530

    added validations in login form and changed styles of form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import react from "react";
+import react, { useState } from "react";
 import Login from "./Login/Login";
 import MainHeader from "./MainHeader/MainHeader";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const loginHandler = () => {
+    setIsLoggedIn(true);
+  };
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
   return (
     <div>
-      <MainHeader />
-      <Login />
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <h1>Welcome to the app!</h1>}
+      </main>
     </div>
   );
 };
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,20 +1,73 @@
-import react from "react";
+import react, { useState } from "react";
+import Button from "../UI/Button/Button";
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 const Login = (props) => {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [isValidPassword, setIsValidPassword] = useState(false);
+  const [isValidForm, setIsValidForm] = useState(false);
+
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+    setIsValidForm(validateEmailHandler && validatePasswordHandler);
+  };
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+    setIsValidForm(validateEmailHandler && validatePasswordHandler);
+  };
+
+  const validateEmailHandler = (email) => {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    setIsValidEmail(re.test(email));
+  };
+  const validatePasswordHandler = (password) => {
+    const re = /^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{8,}$/;
+    setIsValidPassword(re.test(password));
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.onLogin(enteredEmail, enteredPassword);
+  };
+
   return (
     <Card className={classes.login}>
-      <form>
-        <div className={classes.control}>
-          <label>E-Mail</label>
-          <input type="email" />
+      <form onSubmit={submitHandler}>
+        <div
+          className={`${classes.control} ${
+            isValidEmail === false ? classes.invalid : ""
+          }`}
+        >
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            value={enteredEmail}
+            onChange={emailChangeHandler}
+            onBlur={validateEmailHandler}
+          />
         </div>
-        <div className={classes.control}>
-          <label>Password</label>
-          <input type="password" />
+        <div
+          className={`${classes.control} ${
+            isValidPassword === false ? classes.invalid : ""
+          }`}
+        >
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={enteredPassword}
+            onChange={passwordChangeHandler}
+            onBlur={validatePasswordHandler}
+          />
         </div>
         <div className={classes.actions}>
-          <button type="submit">Login</button>
+          <Button type="submit" className={classes.btn} disabled={!isValidForm}>
+            Login
+          </Button>
         </div>
       </form>
     </Card>
